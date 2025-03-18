8974744856bcc63148092fa369bf14de65d504cb
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-18 21:05:58 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,16 +1,16 @@
 chrome.commands.onCommand.addListener(command =>
-  chrome.tabs.query({ active: !0, currentWindow: !0 }).then(tabs => {
+  chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
     let tabLen = tabs.length;
     if (tabLen) {
       let tab = tabs[0];
       let { id, pinned } = tab;
-      command == "1"
-        ? pinned
-          ? chrome.tabs.query({ currentWindow: !0 }, t =>
-              t.length == tabLen && chrome.windows.remove(tab.windowId)
-            )
-          : chrome.tabs.remove(id)
-      :  chrome.tabs.update(id, { pinned: !pinned });
+      try {
+        command == "1"
+          ? pinned
+            ? chrome.tabs.query({ currentWindow: !0 }, t => t.length == tabLen && chrome.windows.remove(tab.windowId))
+            : chrome.tabs.remove(id)
+          : chrome.tabs.update(id, { pinned: !pinned });
+      } catch (e) {}
     }
-  }).catch(() => 0)
+  })
 );
