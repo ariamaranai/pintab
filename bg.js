508e299c173d0dc588104ba3d66536d2e1e30558
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-07 05:03:13 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,22 +1,19 @@
 chrome.commands.onCommand.addListener(command =>
   chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
-    let tabLen = tabs.length;
-    if (tabLen) {
-      let tab = tabs[0];
-      let { id, pinned } = tab;
+    if (tabs.length) {
+      let { id, pinned } = tabs[0];
       command == "1"
         ? pinned
-          ? chrome.tabs.query({ currentWindow: !0 }, t =>
-              t.length == tabLen &&
-              chrome.windows.remove(tab.windowId).catch(() => 0)
-            )
+          ? chrome.tabs.query({ currentWindow: !0 }, t => {
+              if (t.length == 1) {
+                let f = () => chrome.tabs.onActivated.removeListener(f);
+                chrome.tabs.onActivated.hasListeners(f)
+                  ? chrome.windows.remove(t[0].windowId)
+                  : chrome.tabs.onActivated.addListener(f);
+              }
+            })
           : chrome.tabs.remove(id).catch(() => 0)
         : chrome.tabs.update(id, { pinned: !pinned });
-    } else
-      chrome.windows.getLastFocused({ windowTypes: ["devtools"] }, window =>
-        chrome.windows.update(window.id, {
-          state: "minimized"
-        })
-      );
+    }
   })
 );
